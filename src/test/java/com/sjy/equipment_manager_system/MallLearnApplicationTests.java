8f86e351004b21e9202a6c4c9326bd5a4dd4a314
Brian Oster <xstress@outlook.com>
2021-10-25 15:32:20 +0800

    初步模版

---
+++ src/test/java/com/sjy/equipment_manager_system/MallLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjy.equipment_manager_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
